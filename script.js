3c608b18fa3843394a782ff6c0731d82ebbc159f
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-07-17 16:16:42 +0530

    Display book details after adding through button on the main bar. Add some style changes for more responsive design.

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function addBookToLibrary(title, author, pages, read) {
 
 function display() {
     const parent = document.querySelector(".library-container");
+    parent.innerHTML = "";
     for (let i = 0; i < theLibrary.length; i++) {
         const div = document.createElement("div");
         const book = theLibrary[i];
@@ -31,12 +32,34 @@ function display() {
     }
 }
 
-const tolkien = addBookToLibrary("The Hobbit", "Tolkien", 256, false);
-const tolkien2 = addBookToLibrary("The Hobbit", "Tolkien", 256, false);
-display();
+// const tolkien = addBookToLibrary("The Hobbit", "Tolkien", 256, false);
+// const tolkien2 = addBookToLibrary("The Hobbit", "Tolkien", 256, false);
+// display();
 
 // add event listener to add book
 const button = document.querySelector(".add-book");
 button.addEventListener("click", () => {
     document.querySelector("#form-dialog").showModal();
 });
+
+const formButton = document.querySelector("#submit-book");
+formButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.querySelector("#book-form")
+    if (!form.checkValidity()) {
+        form.reportValidity(); // Show built-in validation UI
+        return;
+    }
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const read = document.querySelector("#read").value;
+    if (parseInt(pages) <= 0) {
+    alert("Page count must be positive!");
+    return;
+    }
+    addBookToLibrary(title, author, pages, read);
+    document.querySelector("#form-dialog").close();
+    document.querySelector("#book-form").reset();
+    display();
+})
